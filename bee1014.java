6a4019fb646109d50cad2c5a8b726ee6e1a72af0
Vinicius Lazaroti <viniciuslazarotti@gmail.com>
2026-04-06 20:19:05 -0300

    bee1014.java

---
+++ bee1014.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class bee1008 {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+      
+        System.out.println("Distância: ");
+        int dis = sc.nextInt();
+        System.out.println("Combustível gasto: ");
+        double combGT = sc.nextDouble();
+
+        double combGM = dis/combGT;
+        System.out.println(combGM + " km/l");
+          
+        sc.close();
+ 
+    }
+ 
+}
