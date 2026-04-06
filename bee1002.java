ba8bfb4ad5f1e5fafd289d3476ec1737e5bb8ba9
Vinicius Lazaroti <viniciuslazarotti@gmail.com>
2026-04-06 19:54:05 -0300

    bee1002.java

--- bee1002.java
+++ bee1002.java
@@ -8,7 +8,7 @@ public class bee1002 {
  
         System.out.println("Digite o raio: ");
         double raio = sc.nextDouble();
-        double area = 3.141592 * Math.pow(raio, 2);
+        double area = 3.14159 * Math.pow(raio, 2);
         System.out.println("Área do círculo: " + area);
 
         sc.close();
